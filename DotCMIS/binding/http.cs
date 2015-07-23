0c9b62eff644c3b87ffad364a4876e3335f413dc
Till Lorentzen <till.lorentzen@graudata.com>
2015-07-23 15:28:48 +0200

    Revert "Tiny improvements on http connection creation"
    This reverts commit bb79e241fd64bb25319ebb548023ad4336b8a9c8.
    Used a newer API than it is available in configured .Net

--- DotCMIS/binding/http.cs
+++ DotCMIS/binding/http.cs
@@ -127,7 +127,7 @@ namespace DotCMIS.Binding.Impl
                 for(;;){
 
                     // create connection
-                    HttpWebRequest conn = WebRequest.CreateHttp(url.Url);
+                    HttpWebRequest conn = (HttpWebRequest)WebRequest.Create(url.Url);
                     conn.Method = method;
                     resource.StartResource(conn);
 
