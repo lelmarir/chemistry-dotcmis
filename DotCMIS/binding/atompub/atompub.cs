410da20b98c4869b7013beeb5cebf4985ecbc816
Nicolas Raoul <nicolas.raoul@gmail.com>
2015-08-31 17:15:38 +0900

    Workaround for Documentum enabled

--- DotCMIS/binding/atompub/atompub.cs
+++ DotCMIS/binding/atompub/atompub.cs
@@ -436,14 +436,14 @@ namespace DotCMIS.Binding.AtomPub
 
             if (resp.StatusCode != HttpStatusCode.OK)
             {
-//                // Workaround for Documentum, see https://github.com/aegif/CmisSync/issues/607
-//                string documentumWorkaroundUrl = url.ToString().Replace("cmis:path,", "");
-//                resp = HttpUtils.InvokeGET(new UrlBuilder(documentumWorkaroundUrl), Session);
-//
-//                if (resp.StatusCode != HttpStatusCode.OK)
-//                {
+                // Workaround for Documentum, see https://github.com/aegif/CmisSync/issues/607
+                string documentumWorkaroundUrl = url.ToString().Replace("cmis:path,", "");
+                resp = HttpUtils.InvokeGET(new UrlBuilder(documentumWorkaroundUrl), Session);
+
+                if (resp.StatusCode != HttpStatusCode.OK)
+                {
                     throw ConvertToCmisException(resp);
-//                }
+                }
             }
 
             return resp;
